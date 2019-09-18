9feec3899b9f78d0348a5b35b07a525b66e78c4b
cnboyer <cnboyer@iu.edu>
2019-09-18 09:35:36 -0400

    C2_class918

--- app/public/js/record.app.js
+++ app/public/js/record.app.js
@@ -1,16 +1,40 @@
 var patientRecordApp = new Vue({
   el: '#patientRecordApp',
   data: {
-    patients: []
+    patients: [],
+    formPatient: {
+      firstName:'',
+      lastName:'',
+      dob:'',
+      sexAtBirth:''
+    }
   },
+
+    selectRecord(patient)
+
   methods: {
     fetchPatients() {
       fetch('dummy.php')
       .then(response => response.json())
-      .then(json => { patientRecordApp.patients = json })
+      .then(json => { patientRecordApp.patients = json });
     }
   },
+
+  handleCreateRecord(event) {
+        //TODO: POSET the form contents somewhere
+        //fetch(url)
+        //.then(...)
+        this.patients.push(this.formPatient);
+        this.formPatient: {
+          firstName:'',
+          lastName:'',
+          dob:'',
+          sexAtBirth:''
+      }
+
+
   created() {
     this.fetchPatients();
   }
-});
+}
+);
